ca81f22f04733b21719e4a6a48f3222c37a9e890
TITODATTA <somsubhra109@gmail.com>
2024-04-04 19:57:32 +0530

    some changes

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -13,7 +13,7 @@ from src.mcqgenerator.logger import logging
 
 
 #loading json file
-with open('C:/Users/sodatta/OneDrive - ALLEGIS GROUP/genai_mcq-generator/Response.json','r') as file:
+with open('./Response.json','r') as file:
     RESPONSE_JSON=json.load(file)
 
 #creating a title for the app
